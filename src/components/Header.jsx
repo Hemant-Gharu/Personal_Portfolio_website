b5293946314dae11513df51e2b7b1110be5acb3e
Hemant Gharu <hemantgharu98@gmail.com>
2024-03-08 23:34:33 +0530

    header component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import "../styles/header.css"
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="logo">Logo</div>
+        <div className="nav-links">
+          <ul>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </nav>
+      
+    </>
+  );
+};
+
+export default Header;
